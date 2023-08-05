4fd103024065a047c4491d88a3082e33ca12ebb6
Alejandro <56438011+jalex3421@users.noreply.github.com>
2023-08-05 21:39:07 +0900

    Add files via upload

--- hooks/useMovie.ts
+++ hooks/useMovie.ts
@@ -1,14 +1,13 @@
 import useSWR from 'swr';
 import fetcher from '@/lib/fetcher';
 
-const useBillboard = () => {
-    const {data,error,isLoading} = useSWR('/api/random',fetcher,{
+const useMovie = (id?:string) =>{
+    const {data,error,isLoading} = useSWR(id? `/api/movies/${id}` : null,fetcher,{
         revalidateIfStale: false,
         revalidateOnFocus: false,
-        revalidateOnReconnect: false,
+        revalidateOnReconnect: false
     });
-
     return {data,error,isLoading}
 }
 
-export default useBillboard;
+export default useMovie;
