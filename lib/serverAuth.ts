b1d3389fb8363d7aad2773537210dd60ebac80a9
Alejandro <56438011+jalex3421@users.noreply.github.com>
2023-08-05 21:39:34 +0900

    Add files via upload

--- lib/serverAuth.ts
+++ lib/serverAuth.ts
@@ -7,21 +7,21 @@ import { error } from "console";
 const serverAuth = async (req: NextApiRequest) =>{
     const session = await getSession({req});
 
-    if(!session?.user?.email){
-        throw new Error('Not Signed In')
-    }
-
-    const currentUser = await prismadb.user.findUnique({
+    if (!session?.user?.email) {
+        throw new Error('Not signed in');
+      }
+    
+      const currentUser = await prismadb.user.findUnique({
         where: {
-            email: session.user.email,
+          email: session.user.email,
         }
-    });
-
-    if(!currentUser){
-        throw new Error('Not Signed In')
-    }
-
-    return { currentUser}
-};
-
+      });
+      
+      if (!currentUser) {
+        throw new Error('Not signed in');
+      }
+    
+      return { currentUser };
+}
+    
 export default serverAuth;
